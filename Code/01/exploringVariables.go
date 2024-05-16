30273bab29e361edf351b1eef160428f2b2c0569
Oğuzhan Kara <135004675+oguzkaankaradev@users.noreply.github.com>
2024-05-17 02:30:51 +0900

    01-exploring-variables

---
+++ Code/01/exploringVariables.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//değişken tanımlama birinci method
+	// var greetingText string
+	// greetingText = "Hello world !!!"
+
+	//Değişken tanımlama ikinci method
+	greetingText := "Hello World !!!"
+
+	fmt.Println(greetingText)
+
+}
